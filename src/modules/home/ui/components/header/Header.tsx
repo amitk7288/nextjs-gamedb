e50fcb8955d0f05db5993077bfb1f9034ed8b81b
amitk7288 <amitkadara@gmail.com>
2025-04-03 18:24:13 +0100

    added the search field to header

--- src/modules/home/ui/components/header/Header.tsx
+++ src/modules/home/ui/components/header/Header.tsx
@@ -1,7 +1,9 @@
+import Search from "../search/Search";
+
 export default function Header() {
   return (
     <header id="mainHeader" className="3xl:w-[calc(100%_-_12vw)] dark:border-drkbrd dark:bg-drkbg dark:text-drkcol fixed right-0 top-0 h-[80px] w-full border-b bg-white py-5 pl-[12px] pr-5 lg:w-[calc(100%_-_20vw)] lg:p-5 xl:w-[calc(100%_-_18vw)] 2xl:w-[calc(100%_-_15vw)]">
-      <p className="text-xl font-semibold text-black">this is the header</p>
+      <Search  />
     </header>
   );
 }
